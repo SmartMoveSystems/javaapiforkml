3076df470e2fd12c00272b268c4fe0e201219b74
David Bigham <david.bigham@ebor.com>
2024-09-02 10:43:50 +0930

    try and remove kml prefix on all elements

--- src/main/java/de/micromata/opengis/kml/v_2_2_0/package-info.java
+++ src/main/java/de/micromata/opengis/kml/v_2_2_0/package-info.java
@@ -1,5 +1,5 @@
 @jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.opengis.net/kml/2.2", xmlns = {
-    @jakarta.xml.bind.annotation.XmlNs( prefix = "kml", namespaceURI = "http://www.opengis.net/kml/2.2" )
+    @jakarta.xml.bind.annotation.XmlNs( prefix = "", namespaceURI = "http://www.opengis.net/kml/2.2" )
 },
 elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package de.micromata.opengis.kml.v_2_2_0;
